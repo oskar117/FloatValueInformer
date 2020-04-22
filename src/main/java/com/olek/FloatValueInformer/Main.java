23e55b5946629cb0447fa12452aa3c69c8a50681
oskar117 <olek.konieczny@wp.pl>
2020-04-22 11:24:30 +0200

    init

---
+++ src/main/java/com/olek/FloatValueInformer/Main.java
@@ -0,0 +1,88 @@
+package com.olek.FloatValueInformer;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import java.lang.Thread;
+
+public class Main {
+
+    public static WebDriver driver;
+
+
+    public static void main (String [] args) {
+
+        if (!SystemTray.isSupported()) {
+            System.out.println("SystemTray is not supported");
+            return;
+        }
+        Image image = Toolkit.getDefaultToolkit().getImage("icon.png");
+
+        final PopupMenu popup = new PopupMenu();
+        final TrayIcon trayIcon = new TrayIcon(image, "Float Informer", popup);
+        trayIcon.setImageAutoSize(true);
+        final SystemTray tray = SystemTray.getSystemTray();
+
+        MenuItem getItem = new MenuItem("GET");
+        MenuItem exitItem = new MenuItem("Exit");
+
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(1);
+            }
+        });
+
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addExtensions(new File("extension.crx"));
+        //options.addArguments("--headless");
+        DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
+        desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
+
+        driver = new ChromeDriver(desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        getItem.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                MarketPage page = new MarketPage(driver);
+                page.open("https://steamcommunity.com/market/listings/730/AK-47%20%7C%20Redline%20%28Field-Tested%29");
+
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                page.getFloat();
+                //JOptionPane.showConfirmDialog(null, "twoja stara");
+                System.out.println("Done");
+            }
+        });
+
+        popup.add(exitItem);
+        popup.add(getItem);
+
+        trayIcon.setPopupMenu(popup);
+
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.out.println("TrayIcon could not be added.");
+        }
+
+
+    }
+}
---
+++ src/main/java/com/olek/FloatValueInformer/MarketPage.java
@@ -0,0 +1,30 @@
+package com.olek.FloatValueInformer;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class MarketPage {
+
+    private WebDriver driver;
+
+    public MarketPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void open(String url) {
+        driver.get(url);
+    }
+
+    public void getFloat() {
+        List<WebElement> test = driver.findElements(By.className("floatDropTarget"));
+
+        for(WebElement x : test) {
+            System.out.println("Float: " + x.getText());
+        }
+    }
+}
